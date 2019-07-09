e5b6ccec0e636da527b4e4b5e9d8f98be7af4b8a
gonçalo <gonalo@DESKTOP-5RVGG75>
2019-07-09 14:08:58 +0100

    Implementação do criar e editar concretização

--- TomarCampApp/TomarCampApp/Controllers/ConcretizacaosController.cs
+++ TomarCampApp/TomarCampApp/Controllers/ConcretizacaosController.cs
@@ -40,6 +40,7 @@ namespace TomarCampApp.Controllers
         public ActionResult Create()
         {
             ViewBag.AtividadeFK = new SelectList(db.Atividades, "ID", "Nome");
+            ViewBag.ListaObjetosDeFunc = db.Funcionarios.OrderBy(f => f.Nome).ToList();
             return View();
         }
 
@@ -48,8 +49,32 @@ namespace TomarCampApp.Controllers
         // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,dataInicioConcretizacao,dataFimConcretizacao,local,AtividadeFK")] Concretizacao concretizacao)
+        public ActionResult Create([Bind(Include = "ID,dataInicioConcretizacao,dataFimConcretizacao,local,AtividadeFK")] Concretizacao concretizacao, string[] opcoesEscolhidasDeFunc)
         {
+
+
+            if (opcoesEscolhidasDeFunc == null)
+            {
+                ModelState.AddModelError("", "Necessita escolher pelo menos um Funcionário para associar à concretização.");
+                // gerar a lista de objetos de PA que podem ser associados a Criança
+                ViewBag.ListaObjetosDeFunc = db.Funcionarios.OrderBy(f => f.Nome).ToList();
+
+                // devolver controlo à View
+                return View(concretizacao);
+            }
+
+            List<Funcionarios> listaDeObjetosDeFuncEscolhidos = new List<Funcionarios>();
+            foreach (string item in opcoesEscolhidasDeFunc)
+            {
+
+                Funcionarios f = db.Funcionarios.Find(Convert.ToInt32(item));
+                // adicioná-lo à lista
+                listaDeObjetosDeFuncEscolhidos.Add(f);
+            }
+
+            // adicionar a lista ao objeto de crianças
+            concretizacao.ListaDeObjetosDeFuncionario = listaDeObjetosDeFuncEscolhidos;
+
             if (ModelState.IsValid)
             {
                 db.Concretizacao.Add(concretizacao);
@@ -58,6 +83,7 @@ namespace TomarCampApp.Controllers
             }
 
             ViewBag.AtividadeFK = new SelectList(db.Atividades, "ID", "Nome", concretizacao.AtividadeFK);
+            ViewBag.ListaObjetosDeFunc = db.Funcionarios.OrderBy(f => f.Nome).ToList();
             return View(concretizacao);
         }
 
@@ -74,6 +100,7 @@ namespace TomarCampApp.Controllers
                 return HttpNotFound();
             }
             ViewBag.AtividadeFK = new SelectList(db.Atividades, "ID", "Nome", concretizacao.AtividadeFK);
+            ViewBag.ListaObjetosDeFunc = db.Funcionarios.OrderBy(f => f.Nome).ToList();
             return View(concretizacao);
         }
 
@@ -82,16 +109,74 @@ namespace TomarCampApp.Controllers
         // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,dataInicioConcretizacao,dataFimConcretizacao,local,AtividadeFK")] Concretizacao concretizacao)
+        public ActionResult Edit([Bind(Include = "ID,dataInicioConcretizacao,dataFimConcretizacao,local,AtividadeFK")] Concretizacao concretizacao, string[] opcoesEscolhidasDeFunc)
         {
+
+
+            var conc = db.Concretizacao.Include(f => f.ListaDeObjetosDeFuncionario).Where(f => f.ID == concretizacao.ID).SingleOrDefault();
+
+
+
             if (ModelState.IsValid)
             {
-                db.Entry(concretizacao).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                conc.dataInicioConcretizacao = concretizacao.dataInicioConcretizacao;
+                conc.dataFimConcretizacao = concretizacao.dataFimConcretizacao;
+                conc.local = concretizacao.local;
+                conc.AtividadeFK = concretizacao.AtividadeFK;
+
             }
-            ViewBag.AtividadeFK = new SelectList(db.Atividades, "ID", "Nome", concretizacao.AtividadeFK);
-            return View(concretizacao);
+            else
+            {
+                ViewBag.AtividadeFK = new SelectList(db.Atividades, "ID", "Nome", concretizacao.AtividadeFK);
+                ViewBag.ListaObjetosDeFunc = db.Funcionarios.OrderBy(f => f.Nome).ToList();
+                return View(concretizacao);
+            }
+            // tentar fazer o UPDATE
+            if (TryUpdateModel(conc, "", new string[] { nameof(conc.dataInicioConcretizacao), nameof(conc.dataFimConcretizacao), nameof(conc.local), nameof(conc.AtividadeFK), nameof(conc.ListaDeObjetosDeFuncionario) }))
+            {
+                var elementosDeFunc = db.Funcionarios.ToList();
+
+                if (opcoesEscolhidasDeFunc != null)
+                {
+                    // se existirem opções escolhidas, vamos associá-las
+                    foreach (var ff in elementosDeFunc)
+                    {
+                        if (opcoesEscolhidasDeFunc.Contains(ff.ID.ToString()))
+                        {
+                            // se uma opção escolhida ainda não está associada, cria-se a associação
+                            if (!conc.ListaDeObjetosDeFuncionario.Contains(ff))
+                            {
+                                conc.ListaDeObjetosDeFuncionario.Add(ff);
+                            }
+                        }
+                        else
+                        {
+                            // caso exista associação para uma opção que não foi escolhida, 
+                            // remove-se essa associação
+                            conc.ListaDeObjetosDeFuncionario.Remove(ff);
+                        }
+                    }
+                }
+                else
+                {
+                    // não existem opções escolhidas!
+                    // vamos eliminar todas as associações
+                    foreach (var ff in elementosDeFunc)
+                    {
+                        if (conc.ListaDeObjetosDeFuncionario.Contains(ff))
+                        {
+                            conc.ListaDeObjetosDeFuncionario.Remove(ff);
+                        }
+                    }
+                }
+            }
+
+            // guardar as alterações
+            db.SaveChanges();
+
+            // devolver controlo à View
+            return RedirectToAction("Index");
+
         }
 
         // GET: Concretizacaos/Delete/5
