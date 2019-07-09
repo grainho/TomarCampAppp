235cc2c73b9aa18293f9ab7728fcae59bf65cf13
Jorge Duque <jorgemduque@hotmail.com>
2019-07-09 12:41:07 +0100

    Atividades, display

--- TomarCampApp/TomarCampApp/Models/Atividades.cs
+++ TomarCampApp/TomarCampApp/Models/Atividades.cs
@@ -19,12 +19,16 @@ namespace TomarCampApp.Models
         [Required]
         [RegularExpression(@"^[A-Z][a-z]{1,}[ ][A-Z][a-z]{1,}",
          ErrorMessage = "Apenas primeiro e ultimo nomes")]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
 
+        [Display(Name = "Data de Criação")]
         public DateTime dataCriacao { get; set; }
 
+        [Display(Name = "Materiais Necessários")]
         public string materiais { get; set; }
 
+        [Display(Name = "Descrição da actividades")]
         public string descricao { get; set; }
 
 
